a4c9a46f0d53542971db9b87cbc9bed9e63bd185
1pervysage0 <msyed7008@gmail.com>
2023-06-07 03:25:19 +0530

    Feat(bitespeed):All cases covered.Some edge maybe there

--- db/query.js
+++ db/query.js
@@ -3,6 +3,7 @@ const moment = require("moment");
 
 
 async function getByEmailOrPhone(email, phoneNumber) {
+
     let query = 'SELECT * FROM contact WHERE email = ? OR phoneNumber = ?';
     //console.log(moment().format('yyyy-MM-DD hh:mm:ss.SS'))
     return new Promise((resolve, reject) => {
@@ -19,7 +20,6 @@ async function getByEmailOrPhone(email, phoneNumber) {
 }
 
 async function insertIntoDatabase(data) {
-
     let queryToinsert = 'INSERT into contact SET ?'
     return new Promise((resolve, reject) => {
         connection.query(queryToinsert, [data], (err, data) => {
@@ -39,10 +39,24 @@ async function insertIntoDatabase(data) {
 }
 
 async function getByEmailAndPhone(email, phoneNumber) {
-    let query = 'SELECT * FROM contact WHERE email = ? AND phoneNumber = ?';
+    console.log(email, phoneNumber, "<---in getBYBOTH");
+    let query;
+    let variable = []
+    if (email == null) {
+        query = 'SELECT * FROM contact WHERE phoneNumber = ?'
+        variable = [phoneNumber]
+    }
+    if (phoneNumber == null) {
+        query = 'SELECT * FROM contact WHERE email = ?';
+        variable = [email]
+    }
+    if (email != null && phoneNumber != null) {
+        query = 'SELECT * FROM contact WHERE email = ? AND phoneNumber = ?';
+        variable = [email, phoneNumber]
+    }
     //console.log(moment().format('yyyy-MM-DD hh:mm:ss.SS'))
     return new Promise((resolve, reject) => {
-        connection.query(query, [email, phoneNumber], (err, data) => {
+        connection.query(query, variable, (err, data) => {
             if (err) {
                 console.log("Query Failed", err);
                 reject(err);
@@ -70,4 +84,44 @@ async function getById(id) {
     })
 }
 
-module.exports = { getByEmailOrPhone, insertIntoDatabase, getByEmailAndPhone, getById }
+async function updateLinkPrecedenceById(id, linkedId, linkPrecedence) {
+    const updatedAt = moment().format('yyyy-MM-DD hh:mm:ss.SS ');
+    let query = 'UPDATE contact SET linkPrecedence = ?,linkedId = ?,updatedAt= ? WHERE id = ?';
+    return new Promise((resolve, reject) => {
+        connection.query(query, [linkPrecedence, linkedId, updatedAt, id], (err, data) => {
+            if (err) {
+                console.log("Query Failed", err);
+                reject(err);
+            }
+            //console.log("daTA FROM AND QUERY->>", data[0]);
+            console.log("UPDATEEE DATA--->>>>>>", data)
+            return resolve(data);
+        })
+
+    })
+}
+
+async function getByLinkedId(id) {
+    let query = 'SELECT * FROM contact WHERE linkedId = ?';
+    //console.log(moment().format('yyyy-MM-DD hh:mm:ss.SS'))
+    return new Promise((resolve, reject) => {
+        connection.query(query, [id], (err, data) => {
+            if (err) {
+                console.log("Query Failed", err);
+                reject(err);
+            }
+            //console.log("daTA FROM AND QUERY->>", data[0]);
+            return resolve(data);
+        })
+
+    })
+}
+
+module.exports = {
+    getByEmailOrPhone,
+    insertIntoDatabase,
+    getByEmailAndPhone,
+    getById,
+    updateLinkPrecedenceById,
+    getByLinkedId
+}
--- routes/identity.js
+++ routes/identity.js
@@ -2,17 +2,67 @@ const router = require("express").Router();
 const Query = require("../db/query")
 const moment = require("moment");
 const { all } = require("axios");
+var _ = require('lodash');
 
 router.post("/identify", contactTrace)
 
 async function contactTrace(req, res) {
     const { email, phoneNumber } = req.body;
+    if (email == null && phoneNumber == null) {
+        res.status(400).send({ message: "BAD REQUEST" })
+        return;
+    }
     const allRowsForEmailOrPhone = await Query.getByEmailOrPhone(email, phoneNumber);
-    console.log("allRowsForEmailOrPhone", allRowsForEmailOrPhone);
-    /*If Data Not  Present:-
+    if (email == null && phoneNumber) {
+        console.log("in null email");
+        const uniqueEmailIds = new Set();
+        allRowsForEmailOrPhone.forEach(contact => {
+            uniqueEmailIds.add(contact.email);
+        })
+        const uniqueEmailIdsArr = [...uniqueEmailIds]
 
-        * Make primary COntact
-        * else create secondary contact
+        uniqueEmailIdsArr.forEach(async email => {
+            const dataForEachEmail = await Query.getByEmailAndPhone(email, null);
+            dataForEachEmail.forEach(contact => {
+                if (!allRowsForEmailOrPhone.includes(contact))
+                    allRowsForEmailOrPhone.push(contact);
+            })
+        })
+    }
+    if (email && phoneNumber == null) {
+        console.log("in null phone");
+        const uniquePhonenumbers = new Set();
+        allRowsForEmailOrPhone.forEach(contact => {
+            uniquePhonenumbers.add(contact.phoneNumber);
+        })
+        const uniquePhonenumbersArr = [...uniquePhonenumbers]
+        uniquePhonenumbersArr.forEach(async number => {
+            const dataForEachNumber = await Query.getByEmailAndPhone(null, number);
+            dataForEachNumber.forEach(contact => {
+                if (!allRowsForEmailOrPhone.includes(contact.id))
+                    allRowsForEmailOrPhone.push(contact);
+            })
+        })
+    }
+
+    /*
+    *if email&phone==null then throw error.
+
+    *Get all rows for phone and email.
+        *if phone/email is null:-
+            *Then get all rows again for that email/phone.
+            *With each row again find with the respective email/phone.
+
+    *If Rows Not Present:-
+        * Make primary Contact.
+        
+    * Else :-
+        *Check if primary contact is missing then add through linkedId as id primary key.
+        *If primary contact is present then add all the linkedId.
+        *Find exact match of the request:-
+            *If Present then return the consolidated Response.
+            *If Not Present then create secondary contact and 
+                display the consolidated result along with the inserted data.
         
            */
     if (allRowsForEmailOrPhone.length == 0) {
@@ -37,24 +87,38 @@ async function contactTrace(req, res) {
         }
 
     }
-
     else {
         const primaryPresent = isPrimaryContactPresent(allRowsForEmailOrPhone);
-        console.log("isPRESTNN???", primaryPresent);
-        if (!primaryPresent) {
+        const primaryContacts = primaryPresent.primaryContacts;
+        if (!primaryPresent.primaryPresentFlag) {
             const primaryContactId = allRowsForEmailOrPhone[0].linkedId;
             const primaryContact = await Query.getById(primaryContactId);
             console.log("++++", primaryContact);
             allRowsForEmailOrPhone.push(primaryContact[0]);
+        } else {
+            const linkedContacts = await Query.getByLinkedId(allRowsForEmailOrPhone[0].id);
+            linkedContacts.forEach(contact => {
+                allRowsForEmailOrPhone.push(contact)
+            })
+
         }
 
         const exactData = await Query.getByEmailAndPhone(email, phoneNumber);
-        //console.log("ED--->>", exactData);
+        console.log('EXAACTTTdataa---->', exactData);
+        console.log("allRowsForEmailOrPhone ====>>", allRowsForEmailOrPhone);
+
         if (exactData.length != 0) {
             res.send({ "contact": responseMapper(allRowsForEmailOrPhone) })
         }
         else {
-            if (allRowsForEmailOrPhone) {
+            console.log("primarySwitch", primaryPresent);
+            if (primaryPresent.count > 1) {
+                await changePrimaryToSecondary(primaryContacts);
+                const updatedAllRowsForEmailOrPhone = await Query.getByEmailOrPhone(email, phoneNumber);
+                res.status(200).send({ contact: responseMapper(updatedAllRowsForEmailOrPhone) });
+
+            }
+            else {
                 const linkedId = primaryIdFinder(allRowsForEmailOrPhone);
                 linkPrecedence = "secondary";
                 let dataSecondary = {
@@ -69,14 +133,11 @@ async function contactTrace(req, res) {
                 dataSecondary['id'] = dataForInserted.insertId;
                 allRowsForEmailOrPhone.push(dataSecondary);
                 res.status(200).send({ contact: responseMapper(allRowsForEmailOrPhone) });
-
             }
         }
     }
 
 }
-
-
 
 /*
 {
@@ -89,7 +150,9 @@ async function contactTrace(req, res) {
     }
 */
 function responseMapper(data) {
+    data = _.uniqBy(JSON.parse(JSON.stringify(data)), "id");
     console.log("Data TO MAPP->", data)
+
     const responseData = {
         emails: [],
         phoneNumbers: [],
@@ -104,10 +167,10 @@ function responseMapper(data) {
         }
 
 
-        if (!responseData.emails.includes(contact.email)) {
+        if (!responseData.emails.includes(contact.email) && contact.email != null) {
             responseData['emails'].push(contact.email);
         }
-        if (!responseData.phoneNumbers.includes(contact.phoneNumber)) {
+        if (!responseData.phoneNumbers.includes(contact.phoneNumber) && contact.phoneNumber != null) {
             responseData['phoneNumbers'].push(contact.phoneNumber);
         }
 
@@ -115,7 +178,7 @@ function responseMapper(data) {
     return responseData;
 }
 
-async function primaryIdFinder(data) {
+function primaryIdFinder(data) {
     let id;
     data.forEach(row => {
         if (row.linkPrecedence == 'primary') {
@@ -128,15 +191,41 @@ async function primaryIdFinder(data) {
 
 function isPrimaryContactPresent(data) {
     let primaryPresentFlag = false;
+    let count = 0;
+    let primaryContacts = []
     data.forEach(row => {
-        console.log("ER_>", row);
+        //console.log("ER_>", row);
         if (row.linkPrecedence == 'primary') {
             primaryPresentFlag = true;
-            return;
+            count++;
+            primaryContacts.push(row);
         }
     })
-    return primaryPresentFlag;
+    return {
+        primaryPresentFlag,
+        count,
+        primaryContacts
+    }
 }
+
+async function changePrimaryToSecondary(primaryContacts) {
+    console.log("----Switching primary to SEcondary----");
+    const id = []
+    primaryContacts.forEach(contact => {
+        id.push(contact.id);
+    })
+    id.sort((a, b) => {
+        return a - b;
+    })
+    const primaryId = id[0];
+    id.shift();
+    id.forEach(async id => {
+        await Query.updateLinkPrecedenceById(id, primaryId, "secondary")
+    })
+    return;
+}
+
+
 
 
 
