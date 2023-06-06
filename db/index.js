de91a175108d607aabb183a28b14512706023e99
1pervysage0 <msyed7008@gmail.com>
2023-06-07 03:55:08 +0530

    Feat(bitespeed):Changing sql datasource

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql")
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3307',
-    user: 'root',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
